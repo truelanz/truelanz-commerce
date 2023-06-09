314a2938cc9136d9a0a2ebb78393413b1ce96773
truelanz <devalanoliveira@gmail.com>
2023-06-09 11:22:58 -0300

    first commit

---
+++ src/main/java/com/truelanz/truelanzcommerce/TruelanzCommerceApplication.java
@@ -0,0 +1,13 @@
+package com.truelanz.truelanzcommerce;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TruelanzCommerceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TruelanzCommerceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/truelanz/truelanzcommerce/TruelanzCommerceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.truelanz.truelanzcommerce;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TruelanzCommerceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
