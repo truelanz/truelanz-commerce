ea1706e963c6058c832dcc761eb33b3f20688702
truelanz <devalanoliveira@gmail.com>
2023-06-21 13:47:19 -0300

    added ResponseEntity (status) on controller

--- src/main/java/com/truelanz/truelanzcommerce/controllers/ProductController.java
+++ src/main/java/com/truelanz/truelanzcommerce/controllers/ProductController.java
@@ -1,16 +1,19 @@
 package com.truelanz.truelanzcommerce.controllers;
 
+import java.net.URI;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.truelanz.truelanzcommerce.dto.ProductDTO;
 import com.truelanz.truelanzcommerce.services.ProductService;
@@ -31,20 +34,25 @@ public class ProductController {
     // bucar lista PAGINADA de elementos \\
     // /products?size=5&page=0&sort=name -> QUERY PARAMS, tamanho=, página=, ordenar por= ...
     @GetMapping
-    public Page<ProductDTO> findAll(Pageable pageable) {
-        return productService.findAll(pageable);
+    public ResponseEntity<Page<ProductDTO>> findAll(Pageable pageable) {
+        Page<ProductDTO> dto = productService.findAll(pageable);
+        return ResponseEntity.ok(dto);
     }
     
     // buscar por ID \\
     @GetMapping(value = "/{id}")
-    public ProductDTO findById(@PathVariable Long id) {
-        return productService.findById(id);
+    public ResponseEntity<ProductDTO> findById(@PathVariable Long id) {
+        ProductDTO dto = productService.findById(id);
+        return ResponseEntity.ok(dto);
     }
 
     // insert \\
     @PostMapping
-    public ProductDTO insert(@RequestBody ProductDTO dto) {
-        return productService.insert(dto);
+    public ResponseEntity<ProductDTO> insert(@RequestBody ProductDTO dto) {
+        dto = productService.insert(dto);
+        URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}")
+            .buildAndExpand(dto.getId()).toUri();
+        return ResponseEntity.created(uri).body(dto);
     }
 
 }
